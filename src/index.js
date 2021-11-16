aaceab1288f90e6bff2af387fe8160837fb4c926
Abigail <kristalys.cba4@yahoo.com>
2021-11-16 12:59:30 -0400

    The genesis...

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {Route, BrowserRouter, Routes} from 'react-router-dom';
+import HomePage from "./HomePage";
+import BookMeeting from "./BookMeeting";
+import 'semantic-ui-css/semantic.min.css'
+import UserView from "./UserView";
+import Dashboard from "./Dashboard";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <BrowserRouter>
+        <Routes>
+            <Route exact path="/Home" element={<HomePage/>} />
+            <Route exact path="/UserView" element={<UserView/>} />
+            <Route exact path="/Dashboard" element={<Dashboard/>} />
+        </Routes>
+    </BrowserRouter>,
+    document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
